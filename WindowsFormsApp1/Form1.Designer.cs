5f631feba20d294f2866ea639422b6aa990002bd
Eric Tweedle <erictweeds@gmail.com>
2017-06-26 04:19:45 -0400

    project restart and partway finished

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace RedstoneQuickbooks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+            #region Windows Form Designer generated code
+
+            /// <summary>
+            /// Required method for Designer support - do not modify
+            /// the contents of this method with the code editor.
+            /// </summary>
+            private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Redstone Retail",
+            "Tawse Winery",
+            "Farmers Market"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Customer";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Import CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 118);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 97);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Console Output:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Interop.QBFC13;
+using RedstoneQuickbooks.Session_Framework;
+
+
+namespace RedstoneQuickbooks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.AppendText("Welcome!");
+            connectToQB();
+
+        }
+
+
+        SessionManager sessionManager;
+        private short maxVersion;
+        private string fileStream;
+
+        // CONNECTION TO QB
+        private void connectToQB()
+        {
+            sessionManager = SessionManager.getInstance();
+            maxVersion = sessionManager.QBsdkMajorVersion;
+            textBox1.AppendText("\r\nConnection with Quickbooks established");
+        }
+        private IMsgSetResponse processRequestFromQB(IMsgSetRequest requestSet)
+        {
+            try
+            {
+                //MessageBox.Show(requestSet.ToXMLString());
+                IMsgSetResponse responseSet = sessionManager.doRequest(true, ref requestSet);
+                //MessageBox.Show(responseSet.ToXMLString());
+                return responseSet;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+        private void disconnectFromQB()
+        {
+            if (sessionManager != null)
+            {
+                try
+                {
+                    sessionManager.endSession();
+                    sessionManager.closeConnection();
+                    sessionManager = null;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+        }
+        private IMsgSetRequest buildReportAddRq()
+        {
+            IMsgSetRequest requestMsgSet = sessionManager.getMsgSetRequest();
+            requestMsgSet.Attributes.OnError = ENRqOnError.roeContinue;
+
+            return requestMsgSet;
+        }
+
+        private IMsgSetRequest Build_AddSalesReciept()
+        {
+            IMsgSetRequest requestMsgSet = sessionManager.getMsgSetRequest();
+            requestMsgSet.Attributes.OnError = ENRqOnError.roeContinue;
+
+            ISalesReceiptAdd salesRecieptAdd = requestMsgSet.AppendSalesReceiptAddRq();
+
+            if(comboBox1.Text != "")
+            {
+                salesRecieptAdd.CustomerRef.FullName.SetValue(comboBox1.Text);
+            }
+            else
+            {
+                textBox1.AppendText("\r\nPlease select a customer");
+                return null;
+            }
+
+            if(fileStream != null)
+            {
+                using (var fs = File.OpenRead(fileStream.ToString()))
+                using (var reader = new StreamReader(fs))
+                {
+                    List<string> items = new List<string>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        items.Add(values[4]);
+                        textBox1.AppendText("\r\n" + values[4]);
+                    }
+                }
+            }
+            else
+            {
+                textBox1.AppendText("\r\nPlease select a file first");
+                return null;
+            }
+            return requestMsgSet;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv|All Files (*.*)|*.*";
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if((fileStream = openFileDialog1.FileName) != null)
+                    {
+                        fileStream = openFileDialog1.FileName;
+                        textBox1.AppendText("\r\nSelected: " + fileStream);
+
+                    }
+                }
+                catch(Exception ex)
+                {
+                    textBox1.AppendText("\r\nError: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCustomer = (string)comboBox1.SelectedItem;
+            if(selectedCustomer != "")
+            {
+                textBox1.AppendText("\r\ncustomer changed to: " + selectedCustomer);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Build_AddSalesReciept();
+        }
+    }
+
+}
