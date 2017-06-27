e400825a14b453269bdb50d9f04736fa970cf4c6
Eric Tweedle <erictweeds@gmail.com>
2017-06-27 05:29:10 -0400

    connected with qb, response showing null item error

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.IO;
+using System.Xml;
 using System.Windows.Forms;
 using Interop.QBFC13;
 using RedstoneQuickbooks.Session_Framework;
